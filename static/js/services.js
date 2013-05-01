e607f1de00b901bc9d919eda3d37f2d41a650fe9
Richard Osterloh <richard.osterloh@gmail.com>
2013-05-01 13:26:39 +0100

    Modified services.js

--- static/js/services.js
+++ static/js/services.js
@@ -8,22 +8,22 @@ factory('socket', function ($rootScope) {
   var socket = io.connect();
   return {
     on: function (eventName, callback) {
-	  socket.on(eventName, function () {  
+      socket.on(eventName, function () {  
         var args = arguments;
         $rootScope.$apply(function () {
           callback.apply(socket, args);
         });
-	  });
+      });
     },
     emit: function (eventName, data, callback) {
-	  socket.emit(eventName, data, function () {
+      socket.emit(eventName, data, function () {
         var args = arguments;
         $rootScope.$apply(function () {
           if (callback) {
             callback.apply(socket, args);
           }
         });
-	  })
+      });
     }
   };
 });
