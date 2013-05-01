73df3c4240a21eeba01e243e5970321e1f1d876d
Richard Osterloh <richard.osterloh@gmail.com>
2013-05-01 13:32:53 +0100

    Modified services.js

--- static/js/services.js
+++ static/js/services.js
@@ -1,6 +1,7 @@
 'use strict';
 
-var app = angular.module('app', ['teTouchevents']).
+//var app = angular.module('app', ['teTouchevents']).
+var app = angular.module('app', []).
 
 factory('socket', function ($rootScope) {
   console.log("socket factory");
