6900a57150214641e7d0539143220f9e03c9aa78
Richard Osterloh <richard.osterloh@gmail.com>
2013-05-01 15:31:11 +0100

    Modified handlers.js

--- handlers.js
+++ handlers.js
@@ -148,37 +148,8 @@ function animate(socket, leds) {
     }, 100);
   }
 }
-exports.fixSignature = function(msg){
-    if(! /^a/.test(msg) ) { msg = 'a'+msg; }
-	return msg;
-}
 
-exports.padMessage = function(msg,filler) {
-	if(!filler) { var filler = '-'; }
-	for (var i=0; i<12; i++) { msg += filler; }
-	return msg.substring(0,12);
-}
-
-exports.deviceName = function(msg) {
-	return msg.substring(1,3);
-}
-
-exports.isValid = function(msg) {
-	return msg.length == 12 &&
-		msg.charAt(0) == 'a' &&
-		isAllowedID(exports.deviceName(msg)) &&
-		isAllowedData(msg.substring(3));
-}
-
-var isAllowedID = function(str) {
-	return str.length == 2 &&
-		/[A-Z-#@?\\*]{2}/.test(str);
-}
-
-var isAllowedData = function(str) {
-	return str.length == 9 &&
-		/[0-9A-Z !"#$%&'()*+,\-.\/:;<=>?@[\\\]^_]{9}/.test(str);
-}
+var lhelper = require('./llap_helper');
 
 onDataOverSerial = function(data){
     var msg = data.toString();
