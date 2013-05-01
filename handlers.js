30a8eefac91ca1001004a2f50cff62a6b1af8f98
Richard Osterloh <richard.osterloh@gmail.com>
2013-05-01 15:29:38 +0100

    Modified handlers.js

--- handlers.js
+++ handlers.js
@@ -148,6 +148,50 @@ function animate(socket, leds) {
     }, 100);
   }
 }
+exports.fixSignature = function(msg){
+    if(! /^a/.test(msg) ) { msg = 'a'+msg; }
+	return msg;
+}
+
+exports.padMessage = function(msg,filler) {
+	if(!filler) { var filler = '-'; }
+	for (var i=0; i<12; i++) { msg += filler; }
+	return msg.substring(0,12);
+}
+
+exports.deviceName = function(msg) {
+	return msg.substring(1,3);
+}
+
+exports.isValid = function(msg) {
+	return msg.length == 12 &&
+		msg.charAt(0) == 'a' &&
+		isAllowedID(exports.deviceName(msg)) &&
+		isAllowedData(msg.substring(3));
+}
+
+var isAllowedID = function(str) {
+	return str.length == 2 &&
+		/[A-Z-#@?\\*]{2}/.test(str);
+}
+
+var isAllowedData = function(str) {
+	return str.length == 9 &&
+		/[0-9A-Z !"#$%&'()*+,\-.\/:;<=>?@[\\\]^_]{9}/.test(str);
+}
+
+onDataOverSerial = function(data){
+    var msg = data.toString();
+	// process data received
+	if (lhelper.isValid(msg)) {
+		// log the message
+		logger.log_message(msg);
+		// let all the clients know about the message
+		sockets.emit('received-llap-msg', { content: msg });
+	} else {
+		// message not valid
+	}
+};
 
 // handle socket events
 function handleSocket(socket) {
