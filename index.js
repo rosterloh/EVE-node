d81850efa8e6a049622866d7e56d65143ac896e9
Richard Osterloh <richard.osterloh@gmail.com>
2013-05-02 14:33:41 +0100

    Modified index.js

--- index.js
+++ index.js
@@ -1,3 +1,35 @@
+var express = require('express')
+  , routes = require('./routes')
+  , http = require('http');
+ 
+var app = express();
+var server = app.listen(8888);
+var io = require('socket.io').listen(server);
+ 
+app.configure(function(){
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'jade');
+    app.use(express.favicon());
+    app.use(express.logger('dev'));
+    //app.use(express.compress());
+    app.use(express.static(__dirname + '/static'));
+    // Express Static Middleware Cache-Control
+    //var oneDay = 86400000;
+    //app.use(express.static(__dirname + '/static', { maxAge: oneDay }));
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(app.router);
+});
+ 
+app.configure('development', function(){
+  app.use(express.errorHandler());
+});
+ 
+app.get('/', routes.index);
+ 
+ 
+console.log("Express server listening on port 8888");
+/*
 //var server = require("./server.js");
 //var router = require("./router.js");
 var handlers = require("./handlers.js");
@@ -8,14 +40,10 @@ var app = express()
   , server = http.createServer(app)
   , io = require('socket.io').listen(server);
   
-// Express Static Middleware Cache-Control
-var oneDay = 86400000;
 
-// New call to compress content
-app.use(express.compress());
 
 app.use(express.static(__dirname + '/static'));
-//app.use(express.static(__dirname + '/static', { maxAge: oneDay }));
+
 
 app.listen(process.env.PORT || 8888);
 
@@ -23,7 +51,7 @@ io.set('log level', 1);
 io.sockets.on("connection", handlers.handleSocket );
 console.log("socket started ...");
 server.listen(3000);
-
+*/
 //var handler = {};
 //handler["/leds"] = handlers.leds;
 
