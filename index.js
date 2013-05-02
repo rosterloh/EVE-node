4d5dc45978a25b09b4bbd31bed01d94fede3a883
Richard Osterloh <richard.osterloh@gmail.com>
2013-05-02 11:03:41 +0100

    Modified index.js

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
-var server = require("./server.js");
-var router = require("./router.js");
+//var server = require("./server.js");
+//var router = require("./router.js");
 var handlers = require("./handlers.js");
 var sockets = require("./sockets.js");
+var express = require('express');
+var app = express();
+
+app.use(express.static(__dirname + '/static'));
+
+app.listen(process.env.PORT || 8888);
 
 var handler = {};
 handler["/leds"] = handlers.leds;
