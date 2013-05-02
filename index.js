4d48202b1b01e1f0c56e3e7873ad0fe2d8923d67
Richard Osterloh <richard.osterloh@gmail.com>
2013-05-02 11:11:10 +0100

    Modified index.js

--- index.js
+++ index.js
@@ -4,13 +4,19 @@ var handlers = require("./handlers.js");
 var sockets = require("./sockets.js");
 var express = require('express');
 var app = express();
+// Express Static Middleware Cache-Control
+var oneDay = 86400000;
+
+// New call to compress content
+app.use(express.compress());
 
 app.use(express.static(__dirname + '/static'));
+//app.use(express.static(__dirname + '/static', { maxAge: oneDay }));
 
 app.listen(process.env.PORT || 8888);
 
 var handler = {};
 handler["/leds"] = handlers.leds;
 
-server.start(router.routeHttp, handler);
+//server.start(router.routeHttp, handler);
 sockets.start(server.app, handlers.handleSocket);
