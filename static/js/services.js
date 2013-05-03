a00a63b692377bbc6ce360ce48062770cafd778c
Richard Osterloh <richard.osterloh@gmail.com>
2013-05-03 13:30:11 +0100

    Modified services.js

--- static/js/services.js
+++ static/js/services.js
@@ -2,28 +2,27 @@
 
 //var app = angular.module('app', ['teTouchevents']).
 var app = angular.module('app', []).
-
-  factory('socket', function ($rootScope) {
-    console.log("socket factory");
-    var socket = io.connect();
-    return {
-      on: function (eventName, callback) {
-        socket.on(eventName, function () {  
-          var args = arguments;
-          $rootScope.$apply(function () {
-            callback.apply(socket, args);
-          });
-        });
-      },
-      emit: function (eventName, data, callback) {
-        socket.emit(eventName, data, function () {
-          var args = arguments;
-          $rootScope.$apply(function () {
-            if (callback) {
-              callback.apply(socket, args);
+    factory('socket', function ($rootScope) {
+        console.log("socket factory");
+        var socket = io.connect();
+        return {
+            on: function (eventName, callback) {
+                socket.on(eventName, function () {  
+                    var args = arguments;
+                    $rootScope.$apply(function () {
+                        callback.apply(socket, args);
+                    });
+                });
+            },
+            emit: function (eventName, data, callback) {
+                socket.emit(eventName, data, function () {
+                    var args = arguments;
+                    $rootScope.$apply(function () {
+                        if (callback) {
+                            callback.apply(socket, args);
+                        }
+                    });
+                })
             }
-        });
-      })
-    }
-  };
-});
+        };
+    });
